e4efd48b94d9446585b8351743515463ecbae9fc
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-21 23:19:01 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ export default function routes(app, addon) {
       );
     });
 
-    app.get('/main', addon.authenticate(true), (req, res) => {
+    app.get('/main', (req, res) => {
       // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
       // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
       const {issueKey} = req.query
