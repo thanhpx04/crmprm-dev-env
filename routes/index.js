71cea90f76f6fc03b9ba15bea6b3af6221b79357
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-21 22:20:52 +0700

    up

--- routes/index.js
+++ routes/index.js
@@ -38,19 +38,30 @@ export default function routes(app, addon) {
     app.get('/main', (req, res) => {
       // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
       // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
-      console.log(addon);
-      console.log(req);
-      const {issueKey} = req.query;
-      res.render(
-          'main.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
-          {
-            title: 'Main',
-            issueKey: issueKey
-            //, issueId: req.query['issueId']
-            //, browserOnly: true // you can set this to disable server-side rendering for react views
-          }
-        );
+      const {issueKey} = req.query
+      getIssueSummary(addon, req, issueKey).then((issueSummary) => {
+          res.render(
+              'main.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
+              {
+                  title: 'Main',
+                  issueSummary: issueSummary,
+                  issueKey: issueKey
+                  //, issueId: req.query['issueId']
+                  //, browserOnly: true // you can set this to disable server-side rendering for react views
+              }
+          );
+      })
     });
+
+    async function getIssueSummary (addon, req, issueKey)  {
+        return new Promise((resolve, reject) => {
+
+            var httpClient = addon.httpClient(req);
+            httpClient.get(`/rest/api/3/issue/${issueKey}`, function (err, res, body) {
+                resolve(JSON.parse(body).fields.summary)
+            });
+        })
+    }
 
     // Add additional route handlers here...
 }
