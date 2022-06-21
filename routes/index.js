178d90f566a7db3bb118fb6c99847be18566dae7
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-21 18:19:11 +0700

    try modules

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,29 @@ export default function routes(app, addon) {
 
     // This is an example route used by "generalPages" module (see atlassian-connect.json).
     // Verify that the incoming request is authenticated with Atlassian Connect.
+    app.get('/hello-world', (req, res) => {
+      // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
+      // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
+      const {issueKey} = req.query
+      res.render(
+        'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
+        {
+          title: 'Hello world',
+          issueKey: issueKey
+          //, issueId: req.query['issueId']
+          //, browserOnly: true // you can set this to disable server-side rendering for react views
+        }
+      );
+    });
     app.get('/main', (req, res) => {
-        // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
-        // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
-        res.render(
-          'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
+      // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
+      // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
+      const {issueKey} = req.query
+      res.render(
+          'main.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
           {
-            title: 'Atlassian Connect'
+            title: 'Main',
+            issueKey: issueKey
             //, issueId: req.query['issueId']
             //, browserOnly: true // you can set this to disable server-side rendering for react views
           }
