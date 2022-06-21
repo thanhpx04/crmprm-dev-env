4c8a2d0e16009ac954adb90b773dd550c0781d80
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-21 22:31:49 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,17 @@ export default function routes(app, addon) {
         }
       );
     });
+
+    async function getIssueSummary (addon, req, issueKey)  {
+      return new Promise((resolve, reject) => {
+
+          var httpClient = addon.httpClient(req);
+          httpClient.get(`/rest/api/3/issue/${issueKey}`, function (err, res, body) {
+              resolve(JSON.parse(body).fields.summary)
+          });
+      })
+    };
+
     app.get('/main', (req, res) => {
       // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
       // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
@@ -52,16 +63,6 @@ export default function routes(app, addon) {
           );
       })
     });
-
-    async function getIssueSummary (addon, req, issueKey)  {
-        return new Promise((resolve, reject) => {
-
-            var httpClient = addon.httpClient(req);
-            httpClient.get(`/rest/api/3/issue/${issueKey}`, function (err, res, body) {
-                resolve(JSON.parse(body).fields.summary)
-            });
-        })
-    }
 
     // Add additional route handlers here...
 }
