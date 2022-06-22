eef2a9a5ec9c25e526a3420b5425d58f05d24290
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-22 22:22:55 +0700

    demo authen success

--- routes/index.js
+++ routes/index.js
@@ -5,36 +5,24 @@ export default function routes(app, addon) {
         res.redirect('/atlassian-connect.json');
     });
 
-    // This is an example route used by "generalPages" module (see atlassian-connect.json).
-    // Verify that the incoming request is authenticated with Atlassian Connect.
-    app.get('/hello-world', (req, res) => {
-      // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
-      // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
-      const {issueKey} = req.query;
+    app.get('/hello-world', addon.authenticate(), (req, res) => {
       res.render(
         'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
         {
-          title: 'Hello world',
-          issueKey: issueKey
-          //, issueId: req.query['issueId']
-          //, browserOnly: true // you can set this to disable server-side rendering for react views
+          title: `Hello Thanh's world`
         }
       );
     });
 
-    app.get('/main', addon.authenticate(true), (req, res) => {
-      // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
-      // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
+    app.get('/main', addon.authenticate(), (req, res) => {
       const {issueKey} = req.query
       getIssueSummary(addon, req, issueKey).then((issueSummary) => {
           res.render(
-              'main.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
+              'main.hbs',
               {
                   title: 'Main',
                   issueSummary: issueSummary,
                   issueKey: issueKey
-                  //, issueId: req.query['issueId']
-                  //, browserOnly: true // you can set this to disable server-side rendering for react views
               }
           );
       })
