61b62c115e46603d41d2b5c0503b39fca1b108c5
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-07-13 18:02:01 +0700

    demo cannot use UI kit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 export default function routes(app, addon) {
-    // Redirect root path to /atlassian-connect.json,
-    // which will be served by atlassian-connect-express.
     app.get('/', (req, res) => {
         res.redirect('/atlassian-connect.json');
     });
 
-    app.get('/hello-world', addon.authenticate(), (req, res) => {
+    app.get('/hello-world', (req, res) => {
       res.render(
-        'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
+        'hello-world.jsx',
         {
           title: `Hello Thanh's world`
+        }
+      );
+    });
+
+    app.get('/uikit', (req, res) => {
+      res.render(
+        'uikit-demo.jsx',
+        {
+          title: `Hello uikit demo`
         }
       );
     });
