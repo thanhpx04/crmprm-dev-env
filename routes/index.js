b1213bf9fda18f134360fdc5bef6c870b55c7549
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-07-14 16:35:35 +0700

    finish demo get JWT and request payload

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,15 @@ export default function routes(app, addon) {
     });
 
     app.post('/installed', (req, res) => {
-        console.log("installed-log");
+        console.log("HEADER-LOG");
         console.log(req.headers);
+        console.log("BODY-LOG");
         console.log(req.body);
     });
 
-    app.post('/uninstalled', (req, res) => {
-        console.log("uninstalled");
-        // console.log(addon);
-    });
+    // app.post('/uninstalled', (req, res) => {
+
+    // });
 
     app.get('/hello-world', (req, res) => {
       console.log("hello-world");
